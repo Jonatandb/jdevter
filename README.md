426b4027d6d6161043cee81baef5ee5b1e356592
Jonatandb <jonatandb@gmail.com>
2021-03-07 23:39:17 -0300

    Updating to latest version of Next Favicon added Main logo added Main layout created AppLayout component created Exported jsx styles from AppLayout to styles.js file

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+### Favicon generator: https://www.favicon-generator.org/
+
+### PNG transparenncy creator: https://onlinepngtools.com/create-transparent-png
+
+### Logo creator: https://www.freelogoservices.com/es
+
+---
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,50 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import styles from './Index.module.css'
+import AppLayout from '../components/AppLayout/AppLayout'
+import { colors } from '../styles/theme'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>JDevter 🐦</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href='https://nextjs.org'>jdevter</a>
-        </h1>
-        <nav className={styles.nav}>
-          <Link href='/timeline'>
-            <a>timeline</a>
-          </Link>
-        </nav>
-      </main>
-      <style jsx>{``}</style>
-    </div>
+
+      <AppLayout>
+        <section>
+          <img src='/Jdevter_logo.png' alt='Jdevter main logo' />
+          <h1>Jdevter</h1>
+          <h2>Talk about development<br />with developers 👩🏻‍💻👨🏻‍💻</h2>
+        </section>
+      </AppLayout>
+
+      <style jsx>{`
+        img {
+          width: 120px;
+        }
+
+        section {
+          display: grid;
+          height: 100%;
+          place-content: center;
+          place-items: center;
+        }
+
+        h1 {
+          color: ${colors.primary};
+          font-weight: 800;
+          margin-bottom: 16px;
+        }
+
+        h2 {
+          color: ${colors.secondary};
+          font-size: 21px;
+          margin: 0;
+        }
+      }`}</style>
+
+    </>
   )
 }
 
