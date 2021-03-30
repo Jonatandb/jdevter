59ae5aa006172d39b3e32e781faf2416b9a7872e
Jonatandb <jonatandb@gmail.com>
2021-03-29 23:28:10 -0300

    Code migrated from getInitialProps to getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -4,21 +4,24 @@ export default function JdevitPage(props) {
   return <Jdevit {...props} />
 }
 
-JdevitPage.getInitialProps = context => {
-  const { query, res } = context
-  const { id } = query
-
+export async function getServerSideProps(context) {
+  const { params, res } = context
+  const { id } = params
   const host =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000/'
       : process.env.NEXT_PUBLIC_URL
-
   const url = `${host}api/jdevit/${id}\n`
 
-  return fetch(url).then(apiResponse => {
-    if (apiResponse.ok) return apiResponse.json()
-    if (res) {
-      res.writeHead(301, { Location: '/home' }).end()
+  const apiResponse = await fetch(url)
+
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return {
+      props,
     }
-  })
+  }
+  if (res) {
+    res.writeHead(301, { Location: '/home' }).end()
+  }
 }
